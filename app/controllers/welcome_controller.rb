6cf879e82187d5e112d4640f4aac40dec3d9dff6
Dan <ds5ct@virginia.edu>
2018-07-31 18:39:32 -0400

    Remove social links

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+  	@climbs = Climb.all.limit(3).order("created_at DESC")
+  	@projects = Project.all.limit(3).order("created_at DESC")
   end
 end
